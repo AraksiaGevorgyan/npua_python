611eaab5f340e3ed892fb2c85c5b19f179b10952
Araksia Gevorgyan <araksgevorgyan3@gmail.com>
2023-11-16 22:47:31 +0400

    changes in functions

--- lec_11/main.py
+++ lec_11/main.py
@@ -11,7 +11,7 @@ def filtered_titles():
         filtered_titles = []
 
         for post in posts:
-            if len(post['title'].split()) > 6:
+            if len(post['title'].split()) <= 6:
                 filtered_titles.append(post['title'])
         return filtered_titles
 
@@ -24,9 +24,12 @@ def filtered_body():
     if response.status_code == 200:
         posts = response.json()
         filtered_posts = []
+
         for post in posts:
-            if len(post["body"].split("\n")) > 3:
+            lines = post["body"].splitlines()
+            if len(lines) <= 3:
                 filtered_posts.append(post["body"])
+
         return filtered_posts
     else:
         return f"GET request failed with status code {response.status_code}"
@@ -63,26 +66,23 @@ def delete_post(post_id):
 
 # Example usage for filtered_titles function
 filtered_titles_result = filtered_titles()
-print("Titles with more than 6 words:")
+print("Titles with 6 or fewer words:")
 print(filtered_titles_result)
-print("\n")
 
 # Example usage for filtered_body function
 filtered_body_result = filtered_body()
-print("Posts with more than 3 lines of description:")
+print("\nPosts with 3 or fewer lines of description:")
 print(filtered_body_result)
-print("\n")
 
 # Example usage for create_post function
 created_post = create_post("New Title", "This is the body of the new post.", 1)
 if created_post:
-    print("New Post Created:")
+    print("\nNew Post Created:")
     print(created_post)
-print("\n")
 
 # Example usage for update_post function
 update_post_result = update_post(1, "Updated Title", "This is the updated body of the post.", 1)
-print("Updated post:")
+print("\nUpdated post:")
 print(update_post_result)
 print("\n")
 
